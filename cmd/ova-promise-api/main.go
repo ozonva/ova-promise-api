6918a786f60df0c013f60bcacb58a76ea5c28671
a.kulinskiy <a.kulinskiy@tinkoff.ru>
2021-08-11 12:19:36 +0300

    task-3 A

--- cmd/ova-promise-api/main.go
+++ cmd/ova-promise-api/main.go
@@ -1,8 +1,29 @@
 package main
 
-import "go.uber.org/zap"
+import (
+	"fmt"
+	"os"
+
+	"go.uber.org/zap"
+)
 
 const APIVersion = "0.2.0"
+
+func configReader(filename string) error {
+	f, err := os.Open(filename)
+	if err != nil {
+		return err
+	}
+
+	defer func(f *os.File) {
+		err := f.Close()
+		if err != nil {
+			panic(err)
+		}
+	}(f)
+
+	return nil
+}
 
 func main() {
 	logger, _ := zap.NewProduction()
@@ -12,4 +33,15 @@ func main() {
 	}(logger)
 
 	logger.Info("ova-promise-api", zap.String("version", APIVersion))
+
+	for i := 0; i < 10; i++ {
+		filename := fmt.Sprintf("config-file-%d.cfg", i)
+		if err := configReader(filename); err != nil {
+			logger.Error(
+				fmt.Sprintf("error while reading config from %s", filename),
+				zap.String("filename", filename),
+				zap.Error(err),
+			)
+		}
+	}
 }
