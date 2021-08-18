3016fab97645ae94749631d34e001c99d8282938
a.kulinskiy <a.kulinskiy@tinkoff.ru>
2021-08-19 00:13:54 +0300

    task-4: mock and bdd

---
+++ internal/domain/domain_test.go
@@ -0,0 +1,32 @@
+package domain_test
+
+import (
+	. "github.com/onsi/ginkgo"
+	. "github.com/onsi/gomega"
+
+	"github.com/ozonva/ova-promise-api/internal/domain"
+)
+
+var _ = Describe("Domain", func() {
+	p, _ := domain.NewPromise(domain.GenerateID(), 1, "desc", nil)
+
+	var newPromise domain.Promise
+
+	BeforeEach(func() {
+		newPromise = *p
+	})
+
+	Describe("New promise status", func() {
+		Context("With status", func() {
+			It("should be a `draft`", func() {
+				Expect(newPromise.Status).To(Equal("draft"))
+			})
+		})
+
+		Context("With timestamp", func() {
+			It("CreatedAt should be equal UpdatedAt", func() {
+				Expect(newPromise.CreatedAt).To(Equal(newPromise.UpdatedAt))
+			})
+		})
+	})
+})
