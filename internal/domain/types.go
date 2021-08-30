938f1c2a8e2f037cfd6671f8c45beef6c1a8d5ae
a.kulinskiy <a.kulinskiy@tinkoff.ru>
2021-08-30 08:21:41 +0300

    task-7: review fix

--- internal/domain/types.go
+++ internal/domain/types.go
@@ -1,8 +1,6 @@
 package domain
 
 import (
-	"time"
-
 	"github.com/google/uuid"
 )
 
@@ -12,28 +10,4 @@ type UserID = int64
 
 func GenerateID() ID {
 	return uuid.New()
-}
-
-type NullTime struct {
-	value *time.Time
-}
-
-func (t *NullTime) String() string {
-	if t != nil {
-		if t.value != nil {
-			return t.String()
-		}
-
-		return ""
-	}
-
-	return ""
-}
-
-func (t *NullTime) ToTime() *time.Time {
-	if t.value == nil {
-		return nil
-	}
-
-	return t.value
 }
